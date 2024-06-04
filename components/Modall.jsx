f73385b86045ba7ef6b3c997ceb5e8f02c5e5629
TedrosTK <tedydada@gmail.com>
2024-06-04 16:06:28 -0400

    Event handlers

--- components/Modall.jsx
+++ components/Modall.jsx
@@ -1,13 +1,19 @@
 import './Modall.css'
 
 function Modal({title}) {
+  function cancelClicked(){
+    console.log('Cancel')
+  }
+  function confirmClicked(){
+    console.log('Confirm')
+  }
     return (
         <div>
         <div className="modal">
           <p className="modal__title">{title}</p>
           <div className="modal__buttons">
-            <button className="btn btn__cancel">Cancel</button>
-            <button className="btn">Confirm</button>
+            <button className="btn btn__cancel" onClick={cancelClicked}>Cancel</button>
+            <button className="btn" onClick={confirmClicked}>Confirm</button>
           </div>
         </div>
         <div className="backdrop" />
